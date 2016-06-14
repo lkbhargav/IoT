71f1fe3eed155d5612f7f37947cea7b6f252852e
lkbhargav <lkbhargav9@gmail.com>
2016-06-13 21:48:30 -0400

    first commit

---
+++ src/main/java/com/lkbhargav/pi4led/Application.java
@@ -0,0 +1,15 @@
+package com.lkbhargav.pi4led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Bhargav on 6/13/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/lkbhargav/pi4led/com/lkbhargav/pi4led/controller/controller.java
@@ -0,0 +1,32 @@
+package com.lkbhargav.pi4led.com.lkbhargav.pi4led.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Bhargav on 6/13/2016.
+ */
+@RestController
+public class controller {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting()
+    {
+        return "Sai Baba..";
+    }
+
+    @RequestMapping("/light")
+    public String light()
+    {
+        if(pin == null)
+        {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "MyLED", PinState.LOW);
+        }
+        pin.toggle();
+        return "Light is On";
+    }
+}
